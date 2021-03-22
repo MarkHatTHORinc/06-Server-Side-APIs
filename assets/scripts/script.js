e2f8a8878e8a757820610d628bf80d1f5da08ce7
MarkHatTHORinc <markh@thorinc.com>
2021-03-22 19:10:26 -0400

    Adjust layouts to match mockup more closely

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,8 +5,7 @@ var uviColors = ["green", "green", "green", "yellow", "yellow", "yellow", "orang
 
 async function getGeoInfo(cityName) {
 
-  // var geoInfoUrl = encodeURI(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`);
-  var geoInfoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`;
+  var geoInfoUrl = encodeURI(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`);
 
   // fetch(geoInfoUrl, {
   await fetch(geoInfoUrl, {
@@ -30,123 +29,133 @@ async function getGeoInfo(cityName) {
 }
 
 function getCityWeather(cityName) {
-    var oneCallUrl = encodeURI(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&units=imperial&appid=${apiKey}`);
-    fetch(oneCallUrl, {
-      method: 'GET', //GET is the default.
-      credentials: 'same-origin', // include, *same-origin, omit
-      redirect: 'follow', // manual, *follow, error
+  var oneCallUrl = encodeURI(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&units=imperial&appid=${apiKey}`);
+  fetch(oneCallUrl, {
+    method: 'GET', //GET is the default.
+    credentials: 'same-origin', // include, *same-origin, omit
+    redirect: 'follow', // manual, *follow, error
+  })
+    .then(function (response) {
+      return response.json();
     })
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        //empty divs and ids that we need to dump content into.....
-        console.log(`Weather data => ${data}`);
-        $("#current").empty();
-        var today = moment().format('L');
+    .then(function (data) {
+      // Clear out current weather for search city
+      $("#currentWeather").empty();
+      var today = moment().format('L');
 
-        //create HTML for city information......
-        var cityNameEl = $("<h2>").text(cityName);
-        var todayEl = cityNameEl.append(`&nbsp;${today}`);
-        var tempEL = $("<p>").text("Temperature: " + data.current.temp);
-        var humidityEl = $("<p>").text("Humidity: " + data.current.humidity);
-        var windEl = $("<p>").text("Wind Speed: " + data.current.wind_speed);
-        var currentIcon = buildWeatherIcon(data.current.weather[0].icon);
-        var currentIconEl = $('<img>').attr("src", currentIcon);
-        currentIconEl.attr("style", "height: 60px; width: 60px");
+      // Create Elements for city information
+      var cityNameEl = $("<h2>").text(cityName);
+      var currentIcon = buildWeatherIcon(data.current.weather[0].icon);
+      var currentIconEl = $('<img>').attr("src", currentIcon);
+      currentIconEl.attr("style", "height: 60px; width: 60px");
+      var todayEl = cityNameEl.append(`&nbsp;${today}`, currentIconEl);
+      var tempEL = $("<p>").text(`Temperature: ${data.current.temp} \xB0F`);
+      var humidityEl = $("<p>").text(`Humidity: ${data.current.humidity}%`);
+      var windEl = $("<p>").text(`Wind Speed: ${data.current.wind_speed} MPH`);
 
-        //create HTML div to append new elements to render on page....
-        var newDiv = $('<div>');
+      // Create HTML div to append new elements to render on page....
+      var currentWeatherEl = $('<div>');
+      currentWeatherEl.append(todayEl, tempEL, humidityEl, windEl);
+      $("#currentWeather").html(currentWeatherEl);
 
-        newDiv.append(todayEl, currentIconEl, tempEL, humidityEl, windEl);
+      //Clear out UV Index
+      $("#uvIndex").empty();
+      // Create Elements for UV Index div
+      // var uviEl = $("<button class='btn'>").text("UV Index: " + data.current.uvi);
+      var uviEl = $("<div>").text(`UV Index: `);
+      var uviSpanEl = $("<span>").text(data.current.uvi);
+      var uvi = Math.ceil(data.current.uvi);
+      // make sure we have a valid UV Index
+      if (uvi < 0) {
+        uvi = 0;
+      } else if (uvi > 10) {
+        uvi = 10;
+      } 
+      uviSpanEl.attr("id", "uviNumb");
+      uviSpanEl.attr("class", "uviNumb");
+      uviSpanEl.css("background-color", uviColors[uvi]);
+      uviEl.append(uviSpanEl);
+      let uvIndexEl = $('#uvIndex');
+      uvIndexEl.html(uviEl);
+      // Set the UV Index background color based upon the value
+      // uviEl.css("background-color", uviColors[data.current.uvi]);
+      $("#uviNumb").css("background-color", uviColors[data.current.uvi]);
 
-        $("#current").html(newDiv);
+      // Build the Forecast Section
+      buildForecast(data);
+    });
+}
 
-        $('#uvl-display').empty();
+// Build the weather image
+function buildWeatherIcon(iconName) {
+  return `http://openweathermap.org/img/wn/${iconName}.png`;
+}
 
-        //create HTML for UV Index div
-        var uviEl = $("<button class='btn'>").text("UV Index: " + data.current.uvi);
-        let uvlDisplay = $('#uvl-display');
-        uvlDisplay.html(uviEl);
-        uviEl.css("background-color", uviColors[data.current.uvi]);
+// Build the forecast section
+function buildForecast(data) {
+  // Clear out any previous forecast html elements
+  $("#forecast").empty();
+  // create elements for forecast
+  // start index at 1 because 0 is current day
+  for (var i = 1; i < 6; i++) {
+    // Creating Forecast div
+    var forecastEl = $("<div class='card shadow-lg text-white bg-primary mx-auto mb-10 p-2' style='width: 8.5rem; height: 11rem;'>");
 
-        // Build the Forecast Section
-        buildForecast(data);
-      });
-    }
- 
-  // Build the weather image
-  function buildWeatherIcon(iconName) {
-    return `http://openweathermap.org/img/wn/${iconName}.png`;
+    // Store the responses date temp and humidity
+    var forecastDate = new Date((data.daily[i].dt) * 1000).toLocaleDateString("en-US")
+    var forecastTemp = data.daily[i].temp.max;
+    var forecastHumidity = data.daily[i].humidity;
+
+    // Creating tags with the result items
+    var forecastDateEl = $("<h5 class='card-title'>").text(forecastDate);
+    var forecastTempEl = $("<p class='card-text'>").text(`Temp: ${forecastTemp} \xB0F`);
+    var forecastHumidityEl = $("<p class='card-text'>").text(`Humidity:  ${forecastHumidity}%`);
+
+    var icon = buildWeatherIcon(data.daily[i].weather[0].icon);
+    var forecastIconEl = $('<img>').attr("src", icon);
+    forecastIconEl.attr("style", "height: 40px; width: 40px");
+
+    // Append elements to forecastEl
+    forecastEl.append(forecastDateEl);
+    forecastEl.append(forecastIconEl);
+    forecastEl.append(forecastTempEl);
+    forecastEl.append(forecastHumidityEl);
+    $("#forecast").append(forecastEl);
   }
 
-  // Build the forecast section
-  function buildForecast(data) {
-        //       //empty 5day div--------
-        $("#5day").empty();
-        //       //create HTML for 5day forcast................
-        // start index at 1 because 0 is current day
-              for (var i = 1; i < 6; i++) {
-        //           // Creating a div
-                  var fiveDayDiv = $("<div class='card shadow-lg text-white bg-primary mx-auto mb-10 p-2' style='width: 8.5rem; height: 11rem;'>");
+}
 
-        //           //Storing the responses date temp and humidity.......
-                  var setD = new Date((data.daily[i].dt)*1000).toLocaleDateString("en-US")
-                  var temp = data.daily[i].temp.max;
-                  var hum = data.daily[i].humidity;
-
-        //           //creating tags with the result items information.....
-                  var h5date = $("<h5 class='card-title'>").text(setD);
-                  var pTemp = $("<p class='card-text'>").text("Temp: " + temp);;
-                  var pHum = $("<p class='card-text'>").text("Humidity " + hum);;
-
-        //           var weather = results[i].weather[0].main
-
-                  var icon = buildWeatherIcon(data.daily[i].weather[0].ico);
-                  var iconEl = $('<img>').attr("src", icon);
-                  iconEl.attr("style", "height: 40px; width: 40px");
-
-        //           //append items to.......
-                  fiveDayDiv.append(h5date);
-                  fiveDayDiv.append(iconEl);
-                  fiveDayDiv.append(pTemp);
-                  fiveDayDiv.append(pHum);
-                  $("#5day").append(fiveDayDiv);
-              }
-
-  }
-
-  // Get the Previously Searched Cities
-  function getPreviousSearch() {
-    var previousSearch = JSON.parse(localStorage.getItem("previousSearch"));
-    var searchedEl = $("<button class='btn border text-muted mt-1 shadow-sm bg-white rounded' style='width: 12rem;'>").text(previousSearch);
-    var previousCityEl = $("<div>");
-    previousCityEl.append(searchedEl)
-    $("#searchhistory").prepend(previousCityEl);
-  }
+// Get the Previously Searched Cities
+function getPreviousSearch() {
+  var previousSearch = JSON.parse(localStorage.getItem("previousSearch"));
+  var searchedEl = $("<button class='btn border text-muted mt-1 shadow-sm bg-white rounded' style='width: 12rem;'>").text(previousSearch);
+  var previousCityEl = $("<div>");
+  previousCityEl.append(searchedEl)
+  $("#previousSearch").prepend(previousCityEl);
+}
 
 // Listen for the search button to be clicked
-  $("#searchCity").on("click", function (event) {
-    // Preventing the button from trying to submit the form......
-    event.preventDefault();
-    // Storing the city name........
-    var cityInput = $("#cityInput").val().trim();
+$("#searchCity").on("click", function (event) {
+  // Preventing the button from trying to submit the form......
+  event.preventDefault();
+  // Storing the city name........
+  var cityInput = $("#cityInput").val().trim();
 
-    //save search term to local storage.....
-    var textContent = $(this).siblings("input").val();
-    var cityArray = [];
-    cityArray.push(textContent);
-    localStorage.setItem('previousSearch', JSON.stringify(cityArray));
+  // Save search city to local storage
+  var textContent = $(this).siblings("input").val();
+  var cityArray = [];
+  cityArray.push(textContent);
+  localStorage.setItem('previousSearch', JSON.stringify(cityArray));
 
-    getGeoInfo(cityInput);
-    getPreviousSearch();
-  });
-
-  // Listen for one of the previously searched cities to be clicked
-  $("#searchhistory").on('click', '.btn', function (event) {
-    event.preventDefault();
-    console.log($(this).text());
-    getCityWeather($(this).text());
-  });
-
+  getGeoInfo(cityInput);
   getPreviousSearch();
+});
+
+// Listen for one of the previously searched cities to be clicked
+$("#previousSearch").on('click', '.btn', function (event) {
+  event.preventDefault();
+  console.log($(this).text());
+  getCityWeather($(this).text());
+});
+
+getPreviousSearch();
